7625b5ad7f2dbe3466b010d9aa83c6f5dc0c544e
Serhiy Slipchuk <slipchuks@ukr.net>
2023-05-14 00:09:25 +0300

    prettier formating

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -5,20 +5,20 @@ import FeedbackOptions from './FeedbackOptions/FeedbackOptions';
 import Statistics from './Statistics/Statistics';
 import Notification from './Notification/Notification';
 
-const App = function() {
-  const [ good, setGood] = useState(0);
+const App = function () {
+  const [good, setGood] = useState(0);
   const [neutral, setNeutral] = useState(0);
   const [bad, setBad] = useState(0);
-  
+
   const addFeedback = name => {
-    if(name === 'good') {
-      setGood(prevState => prevState + 1)
+    if (name === 'good') {
+      setGood(prevState => prevState + 1);
     }
-    if(name === 'neutral') {
-      setNeutral(prevState => prevState + 1)
+    if (name === 'neutral') {
+      setNeutral(prevState => prevState + 1);
     }
-    if(name === 'bad') {
-      setBad(prevState => prevState + 1)
+    if (name === 'bad') {
+      setBad(prevState => prevState + 1);
     }
   };
 
@@ -33,42 +33,41 @@ const App = function() {
     return Math.round((good / countTotalFeedback()) * 100);
   };
 
+  return (
+    <div
+      style={{
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        fontSize: 40,
+        color: '#010101',
+      }}
+    >
+      <div className={css.feedback}>
+        <h1 className={css.title}>Caffee "Expresso"</h1>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            options={['good', 'neutral', 'bad']}
+            onLeaveFeedback={addFeedback}
+          />
+        </Section>
 
-    return (
-      <div
-        style={{
-          display: 'flex',
-          justifyContent: 'center',
-          alignItems: 'center',
-          fontSize: 40,
-          color: '#010101',
-        }}
-      >
-        <div className={css.feedback}>
-          <h1 className={css.title}>Caffee "Expresso"</h1>
-          <Section title="Please leave feedback">
-            <FeedbackOptions
-              options={['good', 'neutral', 'bad']}
-              onLeaveFeedback={addFeedback}
+        <Section title="Statistics">
+          {countTotalFeedback() === 0 ? (
+            <Notification message="There is no feedback" />
+          ) : (
+            <Statistics
+              good={good}
+              neutral={neutral}
+              bad={bad}
+              total={countTotalFeedback}
+              positivePercentage={countPositiveFeedbackPercentage}
             />
-          </Section>
-
-          <Section title="Statistics">
-            {countTotalFeedback() === 0 ? (
-              <Notification message="There is no feedback" />
-            ) : (
-              <Statistics
-                good={good}
-                neutral={neutral}
-                bad={bad}
-                total={countTotalFeedback}
-                positivePercentage={countPositiveFeedbackPercentage}
-              />
-            )}
-          </Section>
-        </div>
+          )}
+        </Section>
       </div>
-    );
-}
+    </div>
+  );
+};
 
 export default App;
--- src/components/Notification/Notification.jsx
+++ src/components/Notification/Notification.jsx
@@ -1,15 +1,15 @@
-import css from './Notification.module.scss'
-import PropTypes from 'prop-types'
+import css from './Notification.module.scss';
+import PropTypes from 'prop-types';
 
 const Notification = function ({ message }) {
-    return (
-        <div className={css.statThumb}>
-            <p className={css.notification}>{message}</p>
-        </div>
-    )
-}
+  return (
+    <div className={css.statThumb}>
+      <p className={css.notification}>{message}</p>
+    </div>
+  );
+};
 Notification.propTypes = {
-    message: PropTypes.string.isRequired
-}
+  message: PropTypes.string.isRequired,
+};
 
-export default Notification
+export default Notification;
